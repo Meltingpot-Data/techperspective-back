697e6c124d2d2eb2f7d34a3b4a4ef0115af1f0d6
Dana Huffman <dana.huffman@yahoo.com>
2022-06-01 12:05:43 -0700

    Working on results retrieval

---
+++ modules/getJotformResults.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// RETREIVES RESULTS FROM JOTFORM
+const axios = require('axios');
+
+async function getJotformResults(req, res) {
+  let url = "https://api.jotform.com/form/{req.params.id}/submissions?apiKey={process.env.JOTFORM_API}"
+  const survey = await axios.get(url);
+  console.log(survey);
+}
+
+module.exports = getJotformResults;
